630fea7ef05bc3594a5a775b0afe9772c64cfced
Лев Светлаков <svetlakov_l@bk.ru>
2024-09-27 15:24:16 +0300

    feat/spec-regs

--- main/main.c
+++ main/main.c
@@ -6,27 +6,29 @@ app_main(int argc, char const *argv[])
 {
     // Настройка портов
     int led_gpio = GPIO_NUM_2;
-    int button_gpio = GPIO_NUM_16;
+    // int button_gpio = GPIO_NUM_16;
 
     gpio_reset_pin(led_gpio);
-    gpio_reset_pin(button_gpio);
+    // gpio_reset_pin(button_gpio);
 
     // Пин светодиода - вывод, Пин кнопки - ввод
     gpio_set_direction(led_gpio, GPIO_MODE_OUTPUT);
-    gpio_set_direction(button_gpio, GPIO_MODE_INPUT);
+    // gpio_set_direction(button_gpio, GPIO_MODE_INPUT);
 
     // Изначальный сигнал от кнопки - 1
-    gpio_set_pull_mode(button_gpio, GPIO_PULLUP_ONLY);
+    // gpio_set_pull_mode(button_gpio, GPIO_PULLUP_ONLY);
 
     while(1) {
-        int button_level = gpio_get_level(button_gpio);
+        // int button_level = gpio_get_level(button_gpio);
 
-        if(button_level > 0)
+        // if(button_level > 0)
             gpio_set_level(led_gpio, 0);
-        else
+            
+        sleep(1);
+        // else
             gpio_set_level(led_gpio, 1);
 
-        usleep(500);
+        sleep(1);
     }
 
     return 0;
